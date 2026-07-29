a7f6c60f223840d6a9ff27ec9115d7ade52817fd
renaudlesage <renaudlesage@gmail.com>
2026-07-29 20:15:53 +0200

    Implement routing with BrowserRouter and Routes

--- apps/citoyen/src/App.jsx
+++ apps/citoyen/src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Accueil from './pages/Accueil'
+import Benevole from './pages/Benevole'
 
 export default function App() {
-  return <Accueil />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/benevole" element={<Benevole />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
